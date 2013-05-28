37f8f32af07b47ffe36b9587a610f6126b0e3fef
Tony Butler <tonyjbutler@gmail.com>
2013-05-28 16:47:21 +0100

    Add help string for Learning Object ID field

--- lang/en/nln.php
+++ lang/en/nln.php
@@ -51,9 +51,10 @@ The materials give learners things to do and a chance to think for themselves. T
 This activity module allows teachers to find and add NLN learning objects to their Moodle courses with a minimum of effort.';
 $string['modulenameplural'] = 'NLN learning objects';
 $string['neverseen'] = 'Never seen';
-$string['nln_browse'] = 'Browse the NLN Materials';
-$string['nln_browse_help'] = 'Click this button to view the NLN Materials browser, which lets you browse, preview, and select an NLN learning object.';
+$string['nln_browse'] = 'Browse the NLN materials';
+$string['nln_browse_help'] = 'Click this button to view the NLN materials browser, which lets you browse, preview, and select an NLN learning object.';
 $string['nln_loid'] = 'NLN learning object ID';
+$string['nln_loid_help'] = 'The learning object ID will be filled in automatically once you have chosen a learning object using the Browse button above.';
 $string['nln_required'] = 'Please select an NLN learning object by clicking the button above. If you do not wish to add a learning object, click the Cancel button below.';
 $string['optionsheader'] = 'Options';
 $string['page-mod-nln-x'] = 'Any NLN module page';
