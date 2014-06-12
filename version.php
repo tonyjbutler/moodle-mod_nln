4fd02b35d92f690a0b86517428425b977fab3301
Tony Butler <tonyjbutler@gmail.com>
2014-06-12 16:01:26 +0100

    Bump version number and require Moodle 2.7 release
    Signed-off-by: Tony Butler <tonyjbutler@gmail.com>

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014061200;       // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2013111800;       // Requires this Moodle version
+$plugin->version   = 2014061201;       // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2014051200;       // Requires this Moodle version
 $plugin->component = 'mod_nln';        // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
-$plugin->release   = '2.6.0';
+$plugin->release   = '2.7.0';
 $plugin->maturity  = MATURITY_STABLE;
